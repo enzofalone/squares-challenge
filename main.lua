cbb7c90eb13af83b310426f788c0baf69f94adb2
Enzo Falone <static836319@hotmail.com>
2017-12-12 23:09:28 -0500

    finished menu and empty game.lua added

--- main.lua
+++ main.lua
@@ -1,37 +1,30 @@
 require "collision"
 require "menu"
+require "game"
 function love.load()
-	menu:init();
+   
+   menu:init();
+   print(menu.state);
 end
 
 function love.update(dt)
-	if(state == 0) then
-		menu:update(dt);
-	elseif (state == 1) then
-	
-	end
+   if(menu.state == 0) then
+      menu:update(dt);
+   elseif (menu.state == 1) then
+      game:update(dt);
+   end
 end
 
 function love.draw(t)
-	if(state == 0) then
-		menu:update(dt);
-	elseif (state == 1) then
-		
-	end
+   if menu.state == 0 then
+      menu:draw(t);
+   elseif menu.state == 1 then
+      game:draw(t);
+   end
 end
 
 function love.mousepressed(x, y, button, istouch) 
-	if(state == 0) then
-		menu:mousepressed();
-	end
+   if menu.state == 0 then
+      menu:mousepressed(x, y, button, istouch);
+   end
 end
-
-
-
-
-
-
-
-
-
-
--- menu.lua
+++ menu.lua
@@ -1,64 +1,70 @@
-local menu = {};
-
-bg_menu = nil;
-buttons = {};
-
-state = {
-	current = 0,
-}
+menu = {
+   state = 0,
+};
 
 function menu:init()
-    bg_menu = love.graphics.newImage("assets/menu_bg.png");
-    
-	table.insert(buttons, {
-		name = "start",
-		active = false,
-		x = love.graphics.getWidth() / 2 - 70,
-		y = love.graphics.getHeight() / 2 - 70,
-		img = love.graphics.newImage("assets/bstart.png"),
-		img_a = love.graphics.newImage("assets/bstart_active.png"),
-	})
-	
-	table.insert(buttons, {
-		name = "quit",
-		active = false,
-		x = buttons[1].x,
-		y = buttons[1].y + 100,
-		img = love.graphics.newImage("assets/bquit.png"),
-		img_a = love.graphics.newImage("assets/bquit_active.png"),
-	})
+   self.state = 0;
+   self.bg_menu = love.graphics.newImage("assets/menu_bg.png");
+
+   self.buttons = {};
+   
+   table.insert(self.buttons, {
+		   name = "start",
+		   active = false,
+		   x = love.graphics.getWidth() / 2 - 70,
+		   y = love.graphics.getHeight() / 2 - 70,
+		   img = love.graphics.newImage("assets/bstart.png"),
+		   img_a = love.graphics.newImage("assets/bstart_active.png"),
+			 })
+   
+   table.insert(self.buttons, {
+		   name = "quit",
+		   active = false,
+		   x = self.buttons[1].x,
+		   y = self.buttons[1].y + 100,
+		   img = love.graphics.newImage("assets/bquit.png"),
+		   img_a = love.graphics.newImage("assets/bquit_active.png"),
+			 })
 end
 
 function menu:update(dt)
-    local mouseX, mouseY = love.mouse.getPosition();
-	
-	for i, b in ipairs(buttons) do
-		if collision:check(mouseX, mouseY, 1, 1, b.x, b.y, b.img:getWidth()/2, b.img:getHeight()/2) then
-			b.active = true;
-		else
-			b.active = false;
-		end
-	end
+   local mouseX, mouseY = love.mouse.getPosition();
+   
+   for i, b in ipairs(self.buttons) do
+      if collision:check(mouseX, mouseY, 1, 1, b.x, b.y, b.img:getWidth()/2, b.img:getHeight()/2) then
+	 b.active = true;
+      else
+	 b.active = false;
+      end
+   end
 end
 
 function menu:draw(t)
-    love.graphics.draw(bg_menu, -1, -1, 0, 1.01, 1.01);
-    
-    for i,b in ipairs(buttons) do
-		if not b.active then
-			love.graphics.draw(b.img, b.x, b.y, 0, 0.5, 0.5);
-		else
-			love.graphics.draw(b.img_a, b.x, b.y, 0, 0.5, 0.5);
-		end
-	end
+  love.graphics.draw(self.bg_menu, -1, -1, 0, 1.01, 1.01);
+   
+   for i,b in ipairs(self.buttons) do
+      if not b.active then
+	 love.graphics.draw(b.img, b.x, b.y, 0, 0.5, 0.5);
+      else
+	 love.graphics.draw(b.img_a, b.x, b.y, 0, 0.5, 0.5);
+      end
+   end
+   love.graphics.setColor(0,0,0,255);
+   love.graphics.print("Game made by Enzo Falone", 10, love.graphics.getHeight() - 20);
+   love.graphics.setColor(255,255,255,255);
 end
 
-function love.mousepressed(x, y, button, istouch) 
-	for i, b in ipairs(buttons) do
-		if collision:check(x, y, 1, 1, b.x, b.y, b.img:getWidth()/2, b.img:getHeight()/2) then
-			if b.name == "start" then
-				
-			end
-		end
-	end
+function menu:mousepressed(x, y, button, istouch) 
+   for i, b in ipairs(self.buttons) do
+      if collision:check(x, y, 1, 1, b.x, b.y, b.img:getWidth()/2, b.img:getHeight()/2) then
+	 if b.name == "start" then
+	    menu.state = 1;
+	    game:init();
+	    print("state switched!");
+	 elseif b.name == "quit" then
+	    print("quit!");
+	    love.event.push("quit");
+	 end
+      end
+   end
 end
