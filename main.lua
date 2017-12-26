9e043c3e7ac1159fefe333aca7e5af752d4c52dd
Enzo Falone <static836319@hotmail.com>
2017-12-26 16:42:46 -0500

    trail changes

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 require "src/collision"
+require "src/util"
+require "src/ents/trail"
 require "src/menu"
 require "src/ents/player"
 require "src/game"
@@ -7,37 +9,36 @@ debug = true;
 
 function love.load()
 
-   menu:init();
-   print(menu.state);
-   love.graphics.setBackgroundColor(162,231,177);
+    menu:init();
+    print(menu.state);
+    love.graphics.setBackgroundColor(0, 0, 0);
 end
 
 function love.update(dt)
-   if(menu.state == 0) then
-      menu:update(dt);
-   elseif (menu.state == 1) then
-      game:update(dt);
-   end
+    if(menu.state == 0) then
+        menu:update(dt);
+    elseif (menu.state == 1) then
+        game:update(dt);
+    end
 end
 
 function love.draw(t)
-   if menu.state == 0 then
-      menu:draw(t);
-   elseif menu.state == 1 then
-      game:draw(t);
-   end
+    if menu.state == 0 then
+        menu:draw(t);
+    elseif menu.state == 1 then
+        game:draw(t);
+    end
 
-   if debug then
-      love.graphics.setColor(0,0,0,255);
-      love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), love.graphics.getWidth() - 50, love.graphics.getHeight() - 20);
-      love.graphics.setColor(255,255,255,255);
-   end
+    if debug then
+        love.graphics.setColor(255, 255, 255, 255);
+        love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), love.graphics.getWidth() - 50, love.graphics.getHeight() - 20);
+    end
 end
 
 function love.mousepressed(x, y, button, istouch)
-   if menu.state == 0 then
-      menu:mousepressed(x, y, button, istouch);
-   end
+    if menu.state == 0 then
+        menu:mousepressed(x, y, button, istouch);
+    end
 end
 
 function love.keypressed(key, scancode, isrepeat)
--- src/game.lua
+++ src/game.lua
@@ -1,16 +1,21 @@
 game = {}
 
 function game:init()
-   player:init();
+    game.started = false;
+    player:init();
 end
 
 function game:update(dt)
-   player:update(dt);
+    player:update(dt);
 end
 
 function game:draw()
-   player:draw();
-   love.graphics.setColor(0,0,0,124);
-   --little box above where we will put all the HUD stuff
-   love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), 60);
+    player:draw();
+    love.graphics.setColor(255, 255, 255, 80);
+    --little box above where we will put all the HUD stuff
+    love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), 30);
+
+    if not game.started then
+        love.graphics.printf("Press any arrow key to start the game!", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 400, "center");
+    end
 end
